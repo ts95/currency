91cbc6972a32ba57609a4a2c85cbdcbc162bac48
Toni Sucic <toni.sucic@icloud.com>
2016-03-04 12:11:23 +0100

    Add another formatting option for /convert & use live rates

--- index.js
+++ index.js
@@ -11,12 +11,8 @@ const bot = new TelegramBot(config.bot_token, { polling: true });
 
 console.log("currencybot running");
 
-bot.onText(/\/convert (\w{3}) (\w{3}) (\d+([,\.]\d+)?)/i, function(msg, match) {
-    const convertFrom = match[1].toUpperCase();
-    const convertTo = match[2].toUpperCase();
-    const amount = Number(match[3].replace(',', '.'));
-
-    cc.convert(amount, convertFrom, convertTo)
+function convert(msg, amount, convertFrom, convertTo) {
+    cc.convert(amount, convertFrom, convertTo, true)
         .then(function(result) {
             console.log(`${convertFrom} -> ${convertTo}`);
             console.log(result);
@@ -41,8 +37,22 @@ bot.onText(/\/convert (\w{3}) (\w{3}) (\d+([,\.]\d+)?)/i, function(msg, match) {
                 reply_to_message_id: msg.message_id,
             });
         });
+}
+
+bot.onText(/\/convert (\w{3}) (\w{3}) (\d+([,\.]\d+)?)/i, function(msg, match) {
+    const amount = Number(match[3].replace(',', '.'));
+    const convertFrom = match[1].toUpperCase();
+    const convertTo = match[2].toUpperCase();
+
+    convert(msg, amount, convertFrom, convertTo);
 });
 
-bot.on('message', function(msg) {
-    // console.log(msg);
+bot.onText(/\/convert (\d+([,\.]\d+)?) (\w{3}) (to|in) (\w{3})/i, function(msg, match) {
+    console.log(match);
+
+    const amount = Number(match[1].replace(',', '.'));
+    const convertFrom = match[3].toUpperCase();
+    const convertTo = match[5].toUpperCase();
+
+    convert(msg, amount, convertFrom, convertTo);
 });
